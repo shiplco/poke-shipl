cd1c0eca7d1e59007669b17f39ad08b5eaf55991
aneopsy <paultevatheis@gmail.com>
2019-06-06 23:44:36 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React from 'react'
-import Card from './Card'
-import Pokedex from './Pokedex'
-import styled from 'styled-components'
-import Web3 from 'web3'
-import Shiplwallet from 'shipl-wallet'
+import React from "react";
+import Card from "./Card";
+import Pokedex from "./Pokedex";
+import styled from "styled-components";
+import Web3 from "web3";
+import Shiplwallet from "shipl-wallet";
 
-const appId = '5fc6c72e-db33-4829-9a81-a4227b96238c'
-const targetContractAddress = '0x1c314cc1f12c6ae930385de21024cf32b63ffa0d'
+const appId = "5fc6c72e-db33-4829-9a81-a4227b96238c";
+const targetContractAddress = "0x1c314cc1f12c6ae930385de21024cf32b63ffa0d";
 
 const contractAbi = [
   {
     constant: true,
     inputs: [
       {
-        name: '',
-        type: 'address'
+        name: "",
+        type: "address"
       }
     ],
-    name: 'owners',
+    name: "owners",
     outputs: [
       {
-        name: '',
-        type: 'uint256'
+        name: "",
+        type: "uint256"
       }
     ],
     payable: false,
-    stateMutability: 'view',
-    type: 'function'
+    stateMutability: "view",
+    type: "function"
   },
   {
     constant: false,
     inputs: [
       {
-        name: 'pokemon',
-        type: 'uint256'
+        name: "pokemon",
+        type: "uint256"
       }
     ],
-    name: 'claim',
+    name: "claim",
     outputs: [],
     payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function'
+    stateMutability: "nonpayable",
+    type: "function"
   }
-]
+];
 
 const StyledApp = styled.div`
   font-family: "Press Start 2P";
@@ -130,29 +130,52 @@ const StyledApp = styled.div`
     margin-top: 30px;
     text-align: center;
   }
-`
+
+  @media (max-width: 380px) {
+    .nes-prof,
+    .nes-bulbasaur,
+    .nes-charmander,
+    .nes-squirtle,
+    .nes-pokeball {
+      transform: scale(0.8);
+    }
+
+    p {
+      line-height: 1.2;
+      font-size: 14px;
+    }
+
+    h1 {
+      font-size: 1.5em;
+    }
+
+    .nav-brand {
+      margin: 0px 10px 0px 10px !important;
+    }
+  }
+`;
 
 const cards = [
   {
     id: 1,
-    name: 'Bulbasaur',
-    picture: 'nes-bulbasaur'
+    name: "Bulbasaur",
+    picture: "nes-bulbasaur"
   },
   {
     id: 2,
-    name: 'Charmander',
-    picture: 'nes-charmander'
+    name: "Charmander",
+    picture: "nes-charmander"
   },
   {
     id: 3,
-    name: 'Squirtle',
-    picture: 'nes-squirtle'
+    name: "Squirtle",
+    picture: "nes-squirtle"
   }
-]
+];
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       poke: undefined,
       isLoading: true,
@@ -163,72 +186,72 @@ class App extends React.Component {
       shiplwallet: undefined,
       web3: undefined,
       contract: undefined
-    }
+    };
 
-    this.getAccount = this.getAccount.bind(this)
-    this.claimPokemon = this.claimPokemon.bind(this)
-    this.pokedex = this.pokedex.bind(this)
+    this.getAccount = this.getAccount.bind(this);
+    this.claimPokemon = this.claimPokemon.bind(this);
+    this.pokedex = this.pokedex.bind(this);
   }
 
-  async componentWillMount () {
-    const shiplwallet = await Shiplwallet.create({ appId })
-    const web3 = new Web3(shiplwallet.getWeb3Provider())
-    const contract = new web3.eth.Contract(contractAbi, targetContractAddress)
+  async componentWillMount() {
+    const shiplwallet = await Shiplwallet.create({ appId });
+    const web3 = new Web3(shiplwallet.getWeb3Provider());
+    const contract = new web3.eth.Contract(contractAbi, targetContractAddress);
     this.setState({
       web3,
       contract,
       shiplwallet,
       network: shiplwallet.shiplID.network,
       identity: shiplwallet.shiplID.auth.identity
-    })
+    });
 
     if (shiplwallet.shiplID.auth.identity) {
-      this.pokedex()
+      this.pokedex();
     }
   }
 
-  async getAccount () {
-    const account = (await this.state.web3.eth.getAccounts())[0]
-    this.setState({ identity: this.state.shiplwallet.shiplID.auth.identity })
-    return account
+  async getAccount() {
+    const account = (await this.state.web3.eth.getAccounts())[0];
+    this.setState({ identity: this.state.shiplwallet.shiplID.auth.identity });
+    return account;
   }
 
-  async claimPokemon (pokeId) {
-    const account = await this.getAccount()
+  async claimPokemon(pokeId) {
+    const account = await this.getAccount();
     this.state.contract.methods
       .claim(pokeId)
       .send({ from: account })
-      .on('transactionHash', txHash => this.setState({ txHash }))
-      .on('confirmation', () => this.pokedex())
+      .on("transactionHash", txHash => this.setState({ txHash }))
+      .on("confirmation", () => this.pokedex());
     this.setState({
       isLoading: true,
       catch: cards[pokeId - 1].name,
       txHash: undefined
-    })
-  };
+    });
+  }
 
-  async pokedex () {
-    const account = await this.getAccount()
+  async pokedex() {
+    const account = await this.getAccount();
     const pokeId = await this.state.contract.methods
       .owners(account)
-      .call({ from: account })
-    const poke = cards[pokeId.toNumber() - 1]
-    this.setState({ poke, isLoading: false })
-  };
+      .call({ from: account });
+    const poke = cards[pokeId.toNumber() - 1];
+    this.setState({ poke, isLoading: false });
+  }
 
-  render () {
+  render() {
     return (
       <StyledApp>
         <header>
-          <div className='container'>
-            <div className='nav-brand'>
+          <div className="container">
+            <div className="nav-brand">
               <a
-                href='https://shipl.co'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://shipl.co"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <h1>
-                  <i className='nes-logo brand-logo' />
+                  <i className="nes-logo brand-logo" />
                   Poké Shipl
                 </h1>
               </a>
@@ -236,10 +259,10 @@ class App extends React.Component {
             </div>
           </div>
         </header>
-        <div className='container'>
-          <section className='topic half'>
+        <div className="container">
+          <section className="topic half">
             <div>
-              <h2 id='Information'>#Information</h2>
+              <h2 id="Information">#Information</h2>
               <p>
                 No Ether? No Problem! Shipl enables dApps developers to pay
                 transactions on behalf of their users; they can now interact
@@ -256,22 +279,23 @@ class App extends React.Component {
             />
           </section>
 
-          <section className='showcase'>
-            <div className='nes-container with-title is-centered'>
-              <p className='title'>POKéMON CENTER</p>
-              <section className='message-list'>
-                <section className='message -left'>
+          <section className="showcase">
+            <div className="nes-container with-title is-centered">
+              <p className="title">POKéMON CENTER</p>
+              <section className="message-list">
+                <section className="message -left">
                   <img
-                    src='https://vignette.wikia.nocookie.net/pokemontowerdefensetwo/images/c/cd/Professoroak_icon.png/revision/latest?cb=20130710043109'
-                    alt=''
+                    className="nes-prof"
+                    src="https://vignette.wikia.nocookie.net/pokemontowerdefensetwo/images/c/cd/Professoroak_icon.png/revision/latest?cb=20130710043109"
+                    alt=""
                   />
-                  <div className='nes-balloon from-left'>
+                  <div className="nes-balloon from-left">
                     <p>Now, RED, which POKEMON do you want?</p>
                   </div>
                 </section>
               </section>
-              <div className='nes-container is-rounded'>
-                <div className='card-board'>
+              <div className="nes-container is-rounded">
+                <div className="card-board">
                   {cards.map(e => (
                     <Card key={e.name} item={e} claim={this.claimPokemon} />
                   ))}
@@ -279,24 +303,30 @@ class App extends React.Component {
               </div>
             </div>
           </section>
-
         </div>
         <footer>
           <p>
             <span>©2019 </span>
-            <a href='https://shipl.co' target='_blank' rel='noopener noreferrer'>
+            <a
+              href="https://shipl.co"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               shipl.co
-            </a>{' '}
-            <span>-</span>
-            {' '}
-            <a href='https://github.com/shiplco/poke-shipl' target='_blank' rel='noopener noreferrer'>
+            </a>{" "}
+            <span>-</span>{" "}
+            <a
+              href="https://github.com/shiplco/poke-shipl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               See the code 💻 on github
-            </a>{' '}
+            </a>{" "}
           </p>
         </footer>
       </StyledApp>
-    )
+    );
   }
 }
 
-export default App
+export default App;
